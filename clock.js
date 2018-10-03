27f65fd8fa28c30b9846b32d77831d4a5405bc16
Franco Abaroa <franco9395@gmail.com>
2018-10-03 13:24:03 -0700

    testing cron

--- clock.js
+++ clock.js
@@ -1,20 +1,13 @@
 const axios = require('axios');
 const CronJob = require('cron').CronJob;
+const cryptospotlightScript = require('./cryptospotlightScript').script;
 
 console.log('Before job instantiation');
 const job = new CronJob('* * * * *', function() {
   const d = new Date();
   console.log('Every Minute:', d);
+  cryptospotlightScript();
 });
 console.log('After job instantiation');
-
 job.start();
 
-axios.get('https://cs-price-alerts.herokuapp.com/check/14157012311').then(res => {
-  console.log('res', res);
-}).catch(function (error) {
-  console.log(error);
-});
-
-
-setTimeout(function(){ job.stop(); console.log('done running') }, 300000);
