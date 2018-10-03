b590df4154298b729d5357aa2372bf7317c88dde
Franco Abaroa <franco9395@gmail.com>
2018-10-03 13:08:51 -0700

    t

--- clock.js
+++ clock.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const CronJob = require('cron').CronJob;
 
 console.log('Before job instantiation');
@@ -6,7 +7,14 @@ const job = new CronJob('* * * * *', function() {
   console.log('Every Minute:', d);
 });
 console.log('After job instantiation');
+
 job.start();
+
+axios.get('https://cs-price-alerts.herokuapp.com/check/14157012311').then(res => {
+  console.log('res', res);
+}).catch(function (error) {
+  console.log(error);
+});
 
 
 setTimeout(function(){ job.stop(); console.log('done running') }, 300000);
